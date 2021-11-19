f3683f2421d9cd7ada8327c43266dc8a8efb0fa0
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2021-11-19 01:23:18 -0800

    time setted

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+let today = new Date();
+let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = date + ' ' + time;
+document.getElementById("currentDay").innerHTML = dateTime;
+const saveBtn = document.getElementById("saveBtn");
